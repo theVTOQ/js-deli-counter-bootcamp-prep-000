2d28cbff0cf725ff7b485aa5b76d4dbd44e058d0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-16 01:26:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function takeANumber(line, name){
-  var length = line.length
-  var lengthAsString = length.toString()
+  var length = line.length;
+  var lengthAsString = length.toString();
+  var finalDigit = 
   var suffix;
-  if(length == 1){
+  if(length == 1 ){
     suffix = "st"
   }else if(length == 2){
     suffix = "nd"
